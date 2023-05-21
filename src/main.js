bd388ab4d91b7ec039a08d311eff201a85a94b11
hujaepa <hujaepa@gmail.com>
2023-05-21 22:33:20 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+import "@popperjs/core";
+import "./assets/styles.css";
+import "@fortawesome/fontawesome-free/css/all.css";
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).mount("#app");
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,75 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <nav class="navbar navbar-dark bg-primary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#"> Home </a>
+    </div>
+  </nav>
+  <div class="container-fluid">
+    <div class="row justify-content-center">
+      <div class="col-md-4 m-2">
+        <button class="btn btn-success" @click="downloadData()">
+          <i class="fas fa-download"></i> Download as CSV
+        </button>
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col-md-4">
+        <table class="table" ref="table">
+          <table class="table table-bordered table-light">
+            <thead class="table-success">
+              <th>Asset</th>
+              <th>Department</th>
+            </thead>
+            <tr>
+              <td>Printer</td>
+              <td>HR</td>
+            </tr>
+            <tr>
+              <td>Scanner</td>
+              <td>IT</td>
+            </tr>
+            <tr>
+              <td>Barcode Scanner</td>
+              <td>ACCOUNT</td>
+            </tr>
+          </table>
+        </table>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  methods: {
+    downloadData() {
+      console.log(this.refs);
+      const table = this.$refs.table; // Select the table element
+
+      // Get all rows of the table except the header row
+      const rows = Array.from(table.querySelectorAll("tr"));
+      console.log(rows);
+      // Extract table headers
+      const headers = Array.from(rows.shift().querySelectorAll("th")).map(
+        (header) => header.textContent
+      );
+      // Extract the table data into a 2D array
+      const data = rows.map((row) =>
+        Array.from(row.querySelectorAll("td")).map((cell) => cell.textContent)
+      );
+      // Convert the data to CSV format
+      let csvContent = `${headers.join(",")}\n`;
+      csvContent += data.map((row) => row.join(",")).join("\n");
+
+      // Create a temporary <a> element and set the CSV data as its href
+      const link = document.createElement("a");
+      link.href = "data:text/csv;charset=utf-8," + encodeURI(csvContent);
+      link.download = "table_data.csv"; // Set the filename for the downloaded file
+
+      // Programmatically click the link to trigger the download
+      link.click();
+    },
+  },
+};
 </script>
